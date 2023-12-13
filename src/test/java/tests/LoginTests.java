8db63d7ab66a8781c3159cc64e556729c36118b9
AnnaTesterIs <anna.bokov.is@gmail.com>
2023-12-13 21:13:39 +0200

    loginsucces

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,10 +1,20 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
     @Test
-    public void  test(){
+    public void  loginSuccessTest(){
+app.getHelperUser().openLoginRegistrationForm();
+app.getHelperUser().fillLoginRegistrationForm("test.anna.book@gmail.com", "SAMASAMa2023@");
+app.getHelperUser().submitLogin();
+
+Assert.assertTrue(app.getHelperUser().isLogged());
+        //Assert.assertEquals();
+       // Assert.assertNotEquals();
+        //Assert.assertTrue();
+        //Assert.assertFalse();
 
     }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
 
    @AfterSuite
    public void tearDown(){
-       app.stop();
+
+      //app.stop();
    }
 }
